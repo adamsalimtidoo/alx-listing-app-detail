343ab9d2814d66c8aa02d2df3b526a18845900f8
adamsalimtidoo <119486996+adamsalimtidoo@users.noreply.github.com>
2025-08-03 12:53:08 +0000

    updated

---
+++ interfaces/index.ts
@@ -0,0 +1,16 @@
+//interfaces
+
+export interface ButtonProps {
+  buttonLabel: string;
+  buttonSize?: string;
+  buttonBackgroundColor?: "red" | "blue" | "orange" | "green";
+  action?: () => void;
+}
+
+export interface LayoutProps {
+  children: ReactNode;
+}
+
+export interface PageRouteProps {
+  pageRoute: string;
+}
